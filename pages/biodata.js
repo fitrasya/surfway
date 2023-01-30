148bce9b495db8ebbb60745d99b9f90c6126c853
fitrasya <fakenear77@gmail.com>
2023-01-30 19:21:40 +0700

    update

--- pages/biodata.js
+++ pages/biodata.js
@@ -1,18 +1,43 @@
 import Layout from "../components/Layout";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function survey() {
+  const router = useRouter()
+  const submitContact = async (event) => {
+    event.preventDefault()
+    var formdata = new FormData();
+    formdata.append("nama", event.target.nama.value);
+    formdata.append("nip", event.target.nip.value);
+    formdata.append("instansi", event.target.instansi.value);
+    formdata.append("unit", event.target.unit.value);
+    formdata.append("jabatan", event.target.jabatan.value);
+    formdata.append("telp", event.target.telp.value);
+
+    var requestOptions = {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow'
+    };
+
+    fetch("http://103.150.92.79:1323:1323/api/responden", requestOptions)
+    .then(response => response.json())
+    .then(result => localStorage.setItem("id", result.data))
+    .then(router.push("/substansi"))
+    .catch(error => console.log('error', error));
+  };
+
   return (
     <Layout>
-      <div className="grid grid-cols-2 w-1/3">
-        <form className="col-span-2 px-7 py-5 bg-white shadow-lg text-gray-600">
+      <form className="grid grid-cols-2 w-1/3" onSubmit={submitContact}>
+        <div className="col-span-2 px-7 py-5 bg-white shadow-lg text-gray-600">
           <div className="mb-5 border-b-2 border-sky-500 pb-3 font-medium">
             BIODATA
           </div>
           <div className="mb-5">
             <label className="block m-1 text-sm w-full">Nama Lengkap</label>
             <input
-              type="text"
+              type="text" id="nama"
               className="bg-gray-50 border-sky-500 border text-sm focus:ring-blue-500 focus:border-blue-500 p-2 w-full"
               required
             />
@@ -20,15 +45,7 @@ export default function survey() {
           <div className="mb-5">
             <label className="block m-1 text-sm w-full">NIP</label>
             <input
-              type="text"
-              className="bg-gray-50 border-sky-500 border text-sm focus:ring-blue-500 focus:border-blue-500 p-2 w-full"
-              required
-            />
-          </div>
-          <div className="mb-5">
-            <label className="block m-1 text-sm w-full">Unit Kerja</label>
-            <input
-              type="text"
+              type="text" id="nip"
               className="bg-gray-50 border-sky-500 border text-sm focus:ring-blue-500 focus:border-blue-500 p-2 w-full"
               required
             />
@@ -36,7 +53,15 @@ export default function survey() {
           <div className="mb-5">
             <label className="block m-1 text-sm w-full">Instansi</label>
             <input
-              type="text"
+              type="text" id="instansi"
+              className="bg-gray-50 border-sky-500 border text-sm focus:ring-blue-500 focus:border-blue-500 p-2 w-full"
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label className="block m-1 text-sm w-full">Unit Kerja</label>
+            <input
+              type="text" id="unit"
               className="bg-gray-50 border-sky-500 border text-sm focus:ring-blue-500 focus:border-blue-500 p-2 w-full"
               required
             />
@@ -44,7 +69,7 @@ export default function survey() {
           <div className="mb-4">
             <label className="block m-1 text-sm w-full">Jabatan</label>
             <input
-              type="text"
+              type="text" id="jabatan"
               className="bg-gray-50 border-sky-500 border text-sm p-2 w-full"
               required
             />
@@ -52,12 +77,12 @@ export default function survey() {
           <div className="mb-4">
             <label className="block m-1 text-sm w-full">No. Handphone</label>
             <input
-              type="text"
+              type="text" id="telp"
               className="bg-gray-50 border-sky-500 border text-sm p-2 w-full"
               required
             />
           </div>
-        </form>
+        </div>
         <div className="col-span-2">
           <div className="grid grid-cols-2 gap-4 text-center uppercase text-white mt-5 font-medium">
             <Link href={"/"}>
@@ -66,41 +91,39 @@ export default function survey() {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="2"
+                  strokeWidth="2"
                   stroke="currentColor"
-                  class="w-5 h-5"
+                  className="w-5 h-5"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                   />
                 </svg>
                 Cancel
               </div>
             </Link>
-            <Link href={"/substansi"}>
-              <div className="bg-emerald-500 py-2.5 shadow-lg hover:cursor-pointer hover:bg-emerald-600 flex justify-center gap-2 text-sm">
-                Continue
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="2"
-                  stroke="currentColor"
-                  className="w-5 h-5"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-              </div>
-            </Link>
+            <button type="submit" className="uppercase bg-emerald-500 py-2.5 shadow-lg hover:cursor-pointer hover:bg-emerald-600 flex justify-center gap-2 text-sm">
+              Continue
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </button>
           </div>
         </div>
-      </div>
+      </form>
     </Layout>
   );
 }
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,92 +1,73 @@
 import Layout from "../components/Layout";
 import Link from "next/link";
 
-export default function dashboard() {
+export default function dashboard({respondens}) {
+  console.log(respondens)
   return (
     <Layout>
-      <div className="grid gap-8 text-white uppercase text-sm grid-cols-3 w-2/3 font-medium">
-        <div className="col-span-3 text-xl text-center mb-10">
-          Dashboard Survey Kepuasan Pelanggan<br></br>Direktorat Jabatan ASN
+    <div className="text-white text-sm w-2/3 font-medium">
+      <div className="text-xl text-center mb-16 uppercase">
+        Dashboard Survey Kepuasan Pelanggan<br></br>Direktorat Jabatan ASN
+      </div>
+      <div className="grid uppercase grid-cols-6">
+        <div className="col-span-1 text-sm text-center border-b py-2">
+          NIP
         </div>
-        <Link href={"/pertanyaan2"}>
-          <div className="bg-emerald-500 py-2.5 shadow-lg hover:cursor-pointer hover:bg-emerald-600 flex justify-center gap-2 text-sm">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke="currentColor"
-              class="w-5 h-5"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
-              />
-            </svg>
-            Sangat Puas
-            <div className="bg-emerald-100 text-emerald-600 px-2 rounded-full">
-              17
+        <div className="col-span-1 text-sm text-center border-b py-2">
+          Nama
+        </div>
+        <div className="col-span-1 text-sm text-center border-b py-2">
+          Instansi
+        </div>
+        <div className="col-span-1 text-sm text-center border-b py-2">
+          Unit Kerja
+        </div>
+        <div className="col-span-1 text-sm text-center border-b py-2">
+          Jabatan
+        </div>
+        <div className="col-span-1 text-sm text-center border-b py-2">
+          No. HP
+        </div>
+      </div>
+      { 
+        respondens.map((responden) => {
+          return (
+            <div key={responden.id} className="grid grid-cols-6 font-light border-b">
+              <div className="col-span-1 text-sm text-center py-2">
+                {responden.nip}
+              </div>
+              <div className="col-span-1 text-sm text-center py-2">
+                {responden.nama}
+              </div>
+              <div className="col-span-1 text-sm text-center py-2">
+                {responden.instansi}
+              </div>
+              <div className="col-span-1 text-sm text-center py-2">
+                {responden.unit}
+              </div>
+              <div className="col-span-1 text-sm text-center py-2">
+                {responden.jabatan}
+              </div>
+              <div className="col-span-1 text-sm text-center py-2">
+                {responden.telp}
+              </div>
             </div>
-          </div>
-        </Link>
-        <Link href={"/pertanyaan2"}>
-          <div className="bg-yellow-500 py-2.5 shadow-lg hover:cursor-pointer hover:bg-yellow-600 flex justify-center gap-2 text-sm">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke="currentColor"
-              class="w-5 h-5"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-              />
-            </svg>
-            Cukup Puas
-            <div className="bg-yellow-100 text-yellow-600 px-2 rounded-full">
-              17
-            </div>
-          </div>
-        </Link>
-        <Link href={"/pertanyaan2"}>
-          <div className="bg-rose-500 py-2.5 shadow-lg hover:cursor-pointer hover:bg-rose-600 flex justify-center gap-2 text-sm">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke="currentColor"
-              class="w-5 h-5"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M15.182 16.318A4.486 4.486 0 0012.016 15a4.486 4.486 0 00-3.198 1.318M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
-              />
-            </svg>
-            Kurang Puas
-            <div className="bg-rose-100 text-rose-600 px-2 rounded-full">
-              17
-            </div>
-          </div>
-        </Link>
-        <Link href={"/"} className="col-start-2 mt-10">
+        )})
+      }
+      <div className="grid gap-8 text-white uppercase text-sm grid-cols-3 font-medium">
+        <Link href={"/"} className="col-start-2 mt-16">
           <div className="bg-sky-500 py-2.5 shadow-lg hover:cursor-pointer hover:bg-sky-600 flex justify-center gap-2 text-sm">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="2"
+              strokeWidth="2"
               stroke="currentColor"
-              class="w-5 h-5"
+              className="w-5 h-5"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg>
@@ -94,6 +75,20 @@ export default function dashboard() {
           </div>
         </Link>
       </div>
+    </div>
     </Layout>
   );
+}
+
+export const getStaticProps = async () => {
+  const response = await fetch("http://103.150.92.79:1323/api/responden")
+  const respondens = await response.json()
+
+  console.log(respondens)
+
+  return {
+    props: {
+      respondens: respondens.data
+    }
+  }
 }
